e9e15ca2a08155a222778a554029d4d124035ac8
frerom <roman.fredrik@gmail.com>
2012-12-09 14:12:55 +0000

    Work in progress

--- src/euler074/core.clj
+++ src/euler074/core.clj
@@ -1,1 +1,23 @@
 (ns euler074.core)
+
+(defn integer-to-list [integer] 
+  (map #(- (int %) (int \0)) (str integer)))
+
+(defn factorial [n]
+  (let [r (range 1 (inc n))]
+  (if (= n 0)
+    1
+    (reduce * r))))
+
+(defn sum-of-factorials [integers]
+  (reduce + (map factorial integers)))
+
+(defn- linear-contains? [coll target]
+  (if (some #{target} coll) true false))
+
+(defn non-repeating-terms [starting-number]
+  (defn recursion [mjao]
+    (let [next-term  (sum-of-factorials (integer-to-list starting-number))]
+    (if (some }
+    )
+  (recursion (list starting-number)))
--- test/euler074/core_test.clj
+++ test/euler074/core_test.clj
@@ -4,3 +4,39 @@
 
 (fact "midje seems to work"
   (+ 1 2) => 3)
+
+(fact
+  (non-repeating-terms 145) => '(145)
+  (provided
+    (integer-to-list 145) => '(1 4 5)
+    (sum-of-factorials '(1 4 5)) => 145))
+
+(fact
+  (sum-of-factorials '(1 4 5)) => 145
+  (provided
+    (factorial 1) => 1
+    (factorial 4) => 24
+    (factorial 5) => 120))
+
+(fact
+  (factorial 0) => 1
+  (factorial 1) => 1
+  (factorial 2) => 2
+  (factorial 3) => 6
+  (factorial 9) => 362880)
+
+(fact
+  (sum-of-factorials '(6 9)) => 363600
+  (provided 
+    (factorial 6) => 720
+    (factorial 9) => 362880))
+
+(fact
+  (non-repeating-terms 540) => '(540 145)
+  (provided
+    (integer-to-list 540) => '(5 4 0)))
+
+(future-fact
+  (non-repeating-terms 78) => '(78 45360 871 45361)
+  (provided
+    (integer-to-list 78) => '(7 8)))
